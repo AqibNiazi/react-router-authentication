ce1609758bc1da18ee61255d0435a25324a7c6c0
AqibNiazi <aqibjaved5201@gmail.com>
2025-08-09 23:24:02 +0500

    Events Application Home page upgraded

--- frontend/src/page/Home.jsx
+++ frontend/src/page/Home.jsx
@@ -1,7 +1,10 @@
-import React from "react";
-
+import PageContent from "../components/PageContent";
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <PageContent title="Welcome!">
+      <p>Browse all our amazing Events!</p>
+    </PageContent>
+  );
 };
 
 export default HomePage;
