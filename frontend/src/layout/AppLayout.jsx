a20e3342ed8d4f05ee068901172da6c7bc59d27b
AqibNiazi <aqibjaved5201@gmail.com>
2025-08-22 01:18:02 +0500

    user expiration timer added

--- frontend/src/layout/AppLayout.jsx
+++ frontend/src/layout/AppLayout.jsx
@@ -1,7 +1,28 @@
+import { useEffect } from "react";
 import MainNavigation from "../components/MainNavigation";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLoaderData, useSubmit } from "react-router-dom";
+import { getTokenDuration } from "../util/auth";
 
 const AppLayout = () => {
+  const token = useLoaderData();
+  const submit = useSubmit();
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    if (token === "EXPIRED") {
+      submit(null, { method: "POST", action: "/logout" });
+      return;
+    }
+
+    const tokenDuration = getTokenDuration();
+    console.log(tokenDuration);
+
+    setTimeout(() => {
+      submit(null, { method: "POST", action: "/logout" });
+    }, tokenDuration); // 1 hour
+  }, [token, submit]);
   return (
     <>
       <MainNavigation />
